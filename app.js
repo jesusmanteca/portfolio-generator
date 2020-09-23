52abaf5ca79f18a66cb7a322a02c42cafe77ce76
Jesus Manteca <jesusmanteca@gmail.com>
2020-09-23 15:31:01 -0500

    Dynamically created an HTML format using template lateral and protracting the code to a new file, requiring it in the app

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-var profileDataArr = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-//   };
+// // STORE THE INPUT IN VALUE AS ARRAY STARTING AT INDEX 2 UNTIL THE LENGTH OF THE INDEX, SO... UNTIL YOU'RE DONE TYPING
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//=
+const [name, github] = profileDataArgs
+
+// this is what we want inside the file we're creating, this is the stamp carving
+
+
+// WRITE FILE
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
   
-//   printProfileData(profileDataArr);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+
+
+// var profileDataArr = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArr)
 
 // console.log("");
-// console.log('========2========');
+// console.log('=================');
+
+// //using the variable, we're going to do a for loop that goes through each item and print it in rows
+// profileDataArr.forEach(index => console.log(index));
+
+// console.log('=================');
 // console.log("");
 
-// profileDataArr.forEach(function(profileItem) {
-//   console.log(profileItem);
-// });
+// const generatePage = () => 'Name: Jesus Manteca, Github: jesushub@abc.com';
+// console.log(generatePage())
 
-console.log("");
-console.log('=================');
-console.log("");
-
-//using the variable, we're going to do a for loop that goes through each item
-profileDataArr.forEach(index => console.log(index));
-
-console.log("");
-console.log('=================');
-console.log("");
-
-var myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs)
-
-console.log("");
-console.log('=================');
-console.log("");
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
