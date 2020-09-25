c40342fe166fe72b6657280fff7d2db439740170
Jesus Manteca <jesusmanteca@gmail.com>
2020-09-25 08:41:51 -0500

    inquirer is working

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-// // STORE THE INPUT IN VALUE AS ARRAY STARTING AT INDEX 2 UNTIL THE LENGTH OF THE INDEX, SO... UNTIL YOU'RE DONE TYPING
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(name, github);
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//=
-const [name, github] = profileDataArgs
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-// this is what we want inside the file we're creating, this is the stamp carving
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-
-// WRITE FILE
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
-
-
-// var profileDataArr = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArr)
-
-// console.log("");
-// console.log('=================');
-
-// //using the variable, we're going to do a for loop that goes through each item and print it in rows
-// profileDataArr.forEach(index => console.log(index));
-
-// console.log('=================');
-// console.log("");
-
-// const generatePage = () => 'Name: Jesus Manteca, Github: jesushub@abc.com';
-// console.log(generatePage())
-
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
